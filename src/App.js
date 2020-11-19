2d1e8620cc1185a41f2f9582ba18d77cd08ff531
Anna Thomas <thomasanna85@gmail.com>
2020-11-19 17:13:01 +0900

    Re-add app

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react"
+import Header from "./components/Header"
+import WeatherBox from "./components/WeatherBox"
+console.log(process.env.REACT_APP_API_KEY);
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {cities: [
+                    {name: "Silverton", id: 5752024},  
+                    {name: "Salem", id: 5750162}, 
+                    {name: "Iwanuma", id:2112527}, 
+                    {name: "Hakodate", id:2130188} ],
+                  weather: []  
+                  }
+  }
+
+  componentDidMount(){
+    const IDs = this.state.cities.map(obj=>obj.id).join(",");
+    fetch(`https://api.openweathermap.org/data/2.5/group?id=${IDs}&appid=e3778c28cf2ce53b02d2d30b9347f9af`)
+            .then(response => response.json())
+            .then(response => {
+                const data = response.list;
+                this.setState({weather: data});
+            })
+  }
+
+  render(){
+    console.log(this.state)
+    const content = this.state.weather.map(object => <WeatherBox key={object.id}data={object}/> );
+    return (
+      <div className="App">
+        <Header />
+          <main>
+              <div className="weatherContainer">
+                {content}
+            </div>
+          </main>
+      </div>
+    );
+  }
+}
+
+export default App;
